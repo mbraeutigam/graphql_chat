e4f3f5269054a7fd27c06e52be2f871153e1c521
Marc Bräutigam <marc.braeutigam@novasib.de>
2020-08-21 13:50:28 +0200

    Setup basic server subscription

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+const http = require("http");
+const { ApolloServer, gql, PubSub } = require("apollo-server-express");
 const GraphQLISODate = require("graphql-iso-date");
 const { GraphQLDateTime } = GraphQLISODate;
 
+const PORT = 8000;
+const pubsub = new PubSub();
+
 const messages = [];
+const MESSAGE_ADDED = "MESSAGE_ADDED";
 
 const typeDefs = gql`
   scalar GraphQLDateTime
@@ -22,6 +27,10 @@ const typeDefs = gql`
   type Mutation {
     postMessage(user: String!, content: String!): ID!
   }
+
+  type Subscription {
+    messageAdded: Message
+  }
 `;
 
 const resolvers = {
@@ -30,24 +39,52 @@ const resolvers = {
     messages: () => messages,
   },
   Mutation: {
-    postMessage: (parent, { user, content }) => {
+    postMessage: (parent, args, context) => {
       const id = messages.length;
-      messages.push({
+      const { user, content } = args;
+      const dataset = {
         id,
         user,
         content,
         date: new Date(),
-      });
+      };
+      messages.push(dataset);
+
+      pubsub.publish(MESSAGE_ADDED, { messageAdded: dataset });
       return id;
+    },
+  },
+  Subscription: {
+    messageAdded: {
+      subscribe: () => pubsub.asyncIterator([MESSAGE_ADDED]),
     },
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // subscriptions: {
+  //   onConnect: (connectionParams, webSocket, context) => {
+  //     console.log(webSocket);
+  //   },
+  //   onDisconnect: (webSocket, context) => {
+  //     console.log(webSocket);
+  //   },
+  // },
+});
+
 server.applyMiddleware({ app });
 
-app.listen({ port: 8000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:8000${server.graphqlPath}`)
-);
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`
+  );
+});
