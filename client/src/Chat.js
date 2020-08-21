713b82cc6aa1d335ae41accaf56b0b07639e4501
Marc Bräutigam <marc.braeutigam@novasib.de>
2020-08-21 11:07:36 +0200

    Add message input form and useMutation Hook

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ApolloClient,
   InMemoryCache,
   gql,
   ApolloProvider,
   useQuery,
+  useMutation,
 } from "@apollo/client";
-import { Timeline } from "antd";
+import { Form, Timeline, Input, Button } from "antd";
 
 import "./Chat.css";
 
@@ -23,6 +24,12 @@ const GET_MESSAGES = gql`
       content
       date
     }
+  }
+`;
+
+const POST_MESSAGE = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
   }
 `;
 
@@ -55,13 +62,64 @@ const Messages = ({ user }) => {
   );
 };
 
-function Chat() {
+const ChatInputForm = (props) => {
+  const [form] = Form.useForm();
+  const { user } = props;
+  const [postMessage, { data }] = useMutation(POST_MESSAGE);
+
+  const onFinish = (values) => {
+    console.log(values);
+
+    postMessage({ variables: { user: values.name, content: values.content } });
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  return (
+    <Form layout="inline" form={form} name="chat-input" onFinish={onFinish}>
+      <Form.Item
+        name="name"
+        label="Name"
+        initialValue={user}
+        rules={[{ required: true, message: "Name is required" }]}
+      >
+        <Input placeholder="Please enter a name..." />
+      </Form.Item>
+      <Form.Item
+        name="content"
+        label="Message"
+        rules={[{ required: true, message: "Message is required" }]}
+      >
+        <Input placeholder="Please enter a message..." />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Send Message
+        </Button>{" "}
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const Chat = () => {
+  const [user, setUser] = useState({ user: "Marc", content: "" });
   return (
     <div>
-      <Messages user="Marc" />
+      <div>
+        <Messages user={user.user} />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <ChatInputForm user={user.user} />
+      </div>
     </div>
   );
-}
+};
 
 export default () => (
   <ApolloProvider client={client}>
