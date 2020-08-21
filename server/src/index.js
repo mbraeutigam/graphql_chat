88fbf7389e9da795f050ecb70c32cd919d475490
Marc Bräutigam <marc.braeutigam@novasib.de>
2020-08-21 10:10:51 +0200

    Add date

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
+const GraphQLISODate = require("graphql-iso-date");
+const { GraphQLDateTime } = GraphQLISODate;
 
 const messages = [];
 
 const typeDefs = gql`
+  scalar GraphQLDateTime
+
   type Message {
     id: ID!
     user: String!
     content: String!
+    date: GraphQLDateTime!
   }
 
   type Query {
@@ -20,13 +25,19 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
+  GraphQLDateTime,
   Query: {
     messages: () => messages,
   },
   Mutation: {
     postMessage: (parent, { user, content }) => {
       const id = messages.length;
-      messages.push({ id, user, content });
+      messages.push({
+        id,
+        user,
+        content,
+        date: new Date(),
+      });
       return id;
     },
   },
